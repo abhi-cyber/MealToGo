005e991c9d29728d92b176c21edbcf6685d66963
abhi-cyber <abhirajchatrath@gmail.com>
2023-02-13 12:45:48 +0530

    data transformed

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -10,16 +10,13 @@ export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
   });
 };
 
-const restaurantTransform = (result) => {
-  const newResult = camelize(result);
-  return newResult;
-};
-
-restaurantRequest()
-  .then(restaurantTransform)
-  .then((transformedResponse) => {
-    console.log(transformedResponse);
-  })
-  .catch((err) => {
-    console.log("error");
+const restaurantTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.bussiness_status === "CLOSED_TEMPORARILY",
+    };
   });
+  return camelize(mappedResults);
+};
