06d78a2465136dcb11c63afdebfe00c5b2584235
abhi-cyber <abhirajchatrath@gmail.com>
2023-03-25 22:50:46 +0530

    android CompactImage error fixed

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import { Text } from "../typography/text.component";
+import { WebView } from "react-native-webview";
+import { Platform } from "react-native";
 
 const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebView = styled(WebView)`
   border-radius: 10px;
   width: 120px;
   height: 100px;
@@ -14,10 +22,14 @@ const Item = styled.View`
   align-items: center;
 `;
 
+const isAndroid = Platform.OS === "android";
+
 export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebView : CompactImage;
+
   return (
     <Item>
-      <CompactImage source={{ uri: restaurant.photos[0] }} />
+      <Image source={{ uri: restaurant.photos[0] }} />
       <Text variant="caption" numberOfLines={3}>
         {restaurant.name}
       </Text>
