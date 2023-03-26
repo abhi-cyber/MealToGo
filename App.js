2320481eb6a27dc71e1c55dc455a690c12b47e68
abhi-cyber <abhirajchatrath@gmail.com>
2023-03-26 19:30:17 +0530

    firebase signIn practice

--- App.js
+++ App.js
@@ -12,6 +12,31 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCREbJ29uIGeduyVgh4OL8nFrpy4mT6u_0",
+  authDomain: "mealstogo-5bb69.firebaseapp.com",
+  projectId: "mealstogo-5bb69",
+  storageBucket: "mealstogo-5bb69.appspot.com",
+  messagingSenderId: "653375549258",
+  appId: "1:653375549258:web:3ca757e4b5586717741bb3",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+signInWithEmailAndPassword(auth, "email.2003@gmail.com", "password")
+  .then((userCredential) => {
+    // Signed in
+    console.log("signed in");
+    // ...
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
