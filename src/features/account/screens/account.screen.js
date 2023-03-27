2583fcc37e48b05fd0249bfde1488105088b3c1d
abhi-cyber <abhirajchatrath@gmail.com>
2023-03-27 22:06:21 +0530

    styling

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -4,6 +4,7 @@ import {
   AccountContainer,
   AccountCover,
   AuthButton,
+  Title,
 } from "../components/account.styles";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
@@ -11,6 +12,7 @@ export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
           icon="lock-open-outline"
--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -5,18 +5,21 @@ import {
   AccountContainer,
   AuthButton,
   AuthInput,
+  Title,
+  ErrorContainer,
 } from "../components/account.styles";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { onLogin, error } = useContext(AuthenticationContext);
   return (
     <AccountBackground>
       <AccountCover />
+      <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthInput
           label="E-mail"
@@ -39,7 +42,9 @@ export const LoginScreen = () => {
         </Spacer>
         {error && (
           <Spacer size="large">
-            <Text variant="error">{error}</Text>
+            <ErrorContainer size="large">
+              <Text variant="error">{error}</Text>
+            </ErrorContainer>
           </Spacer>
         )}
         <Spacer size="large">
@@ -52,6 +57,11 @@ export const LoginScreen = () => {
           </AuthButton>
         </Spacer>
       </AccountContainer>
+      <Spacer size="large">
+        <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+          Back
+        </AuthButton>
+      </Spacer>
     </AccountBackground>
   );
 };
