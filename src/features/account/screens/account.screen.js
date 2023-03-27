cc6fc95156c60a91c41e52237e73f768c58ec4b2
abhi-cyber <abhirajchatrath@gmail.com>
2023-03-27 23:33:28 +0530

    Watermelon animation added

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,13 @@
 import React from "react";
+import LottieView from "lottie-react-native";
+
 import {
   AccountBackground,
   AccountContainer,
   AccountCover,
   AuthButton,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
@@ -12,6 +15,15 @@ export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
